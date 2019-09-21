dd73846ced05547674538d352e2317fc0c77e8de
FUKUCHI Haruki <fukuchi.haruki@gmail.com>
2019-09-21 12:56:48 +0900

    feat: インストール用スクリプト

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FILES=".bash_profile .bashrc .gvimrc .screenrc .vimrc"
+for FILE in $FILES
+do
+        test -f $HOME/$FILE && cp -a $HOME/$FILE $HOME/$FILE~
+        curl -L https://raw.githubusercontent.com/fukuchiharuki/dotfiles/master/$FILE > $HOME/$FILE
+done
